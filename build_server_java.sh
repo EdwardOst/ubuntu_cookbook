525da3e6db9d099e1952077d082080dd4933f1fd
Edward Ost <eost@talend.com>
2014-11-22 10:37:59 -0500

    renamed build to download and updated build to invoke scripts

--- build_server_java.sh
+++ build_server_java.sh
@@ -1,4 +1,4 @@
-wget https://raw.githubusercontent.com/EdwardOst/ubuntu_cookbook/master/install_server_basic_util.sh
-wget https://raw.githubusercontent.com/EdwardOst/ubuntu_cookbook/master/install_java.sh
-wget https://raw.githubusercontent.com/EdwardOst/ubuntu_cookbook/master/install_ant.sh
-wget https://raw.githubusercontent.com/EdwardOst/ubuntu_cookbook/master/install_maven.sh
+./install_server_basic_util.sh
+./install_java.sh
+./install_ant.sh
+./install_maven.sh
---
+++ download_server_java.sh
@@ -0,0 +1,4 @@
+wget https://raw.githubusercontent.com/EdwardOst/ubuntu_cookbook/master/install_server_basic_util.sh
+wget https://raw.githubusercontent.com/EdwardOst/ubuntu_cookbook/master/install_java.sh
+wget https://raw.githubusercontent.com/EdwardOst/ubuntu_cookbook/master/install_ant.sh
+wget https://raw.githubusercontent.com/EdwardOst/ubuntu_cookbook/master/install_maven.sh
