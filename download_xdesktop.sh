7a619f26393988e7b00a53ec41eb6b9cdb24552e
Edward Ost <eost@talend.com>
2014-11-24 19:21:58 -0500

    fixed download_xdesktop to download build rather than another download script.

--- download_xdesktop.sh
+++ download_xdesktop.sh
@@ -1,4 +1,4 @@
 wget https://raw.githubusercontent.com/EdwardOst/ubuntu_cookbook/master/install_server_basic_util.sh
 wget https://raw.githubusercontent.com/EdwardOst/ubuntu_cookbook/master/install_xubuntu_desktop.sh
 wget https://raw.githubusercontent.com/EdwardOst/ubuntu_cookbook/master/install_x2go.sh
-wget https://raw.githubusercontent.com/EdwardOst/ubuntu_cookbook/master/download_xdesktop.sh
+wget https://raw.githubusercontent.com/EdwardOst/ubuntu_cookbook/master/build_xdesktop.sh
